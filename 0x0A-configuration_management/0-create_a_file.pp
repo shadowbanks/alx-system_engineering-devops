0603cef3fe274144f2173aa23d0d07eb74b3e8ae
GhostProTech <akanniwilliams@gmail.com>
2023-09-23 03:49:10 +0100

    'Create a file using puppet'

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file using puppet
+
+file {'school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
