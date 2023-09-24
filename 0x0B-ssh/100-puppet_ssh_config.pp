f377275a4bf58df042eeab9a8340d5291ae96a11
GhostProTech <akanniwilliams@gmail.com>
2023-09-24 22:15:17 +0100

    Change resource names

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 # Client Configuration file
-file_line { 'ssh_config':
-  ensure => present,
+file_line { 'ssh_config no password':
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
-  path   => '/etc/ssh/ssh_config',
-}
-file_line { 'ssh_config':
   ensure => present,
-  line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'config_file':
   path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  ensure => present,
 }
