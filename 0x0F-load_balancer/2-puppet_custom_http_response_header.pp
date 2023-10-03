94f2af6de8f33fc90816a584e67e5ca1e6b19d2e
Shadowbanks <akanniwilliams@gmail.com>
2023-10-03 04:09:32 +0100

    'Puppet script to install and configure web server'

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+#install nginx and configure
+str='location / {'
+custom_head="location / {\n\t\tadd_header X-Served-By \$hostname;"
+exec { 'update':
+  command  => 'sudo apt-get update -y',
+  provider => shell,
+}
+
+-> exec { 'install_nginx':
+  command  => 'sudo apt-get install nginx -y',
+  provider => shell,
+}
+
+-> exec { 'custom_header':
+  command  => 'sed -i -z "s|${str}|${custom_head}|" /etc/nginx/sites-available/default',
+  provider => shell,
+}
+
+-> exec { 'restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
