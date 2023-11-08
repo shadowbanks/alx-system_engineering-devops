60d27de2175a5b26176494bc02982ebd86620132
Shadowbanks <akanniwilliams@gmail.com>
2023-11-08 15:31:58 +0100

    Get the total subscribers of any subbreddit specified

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Get the subscribers count of a subreddit
+"""
+import requests
+import time
+
+
+def number_of_subscribers(subreddit):
+    """
+    Subreddit subscribers count
+    """
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    headers = {"User-Agent": "TSubs/1.0 (by Wills)"}
+    res = requests.get(url, headers=headers)
+    if (res.status_code == 200):
+        data = res.json()
+        subs = data["data"]["subscribers"]
+        return subs
+    return 0
